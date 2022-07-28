31dc145908dfe5dea7d41b48449fc2b8f9b9a3b4
nik-dolphin <nikunj.dolphinwebsolution@gmail.com>
2022-07-28 12:15:14 +0530

    add other code

--- src/component/dashboard.js
+++ src/component/dashboard.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { ThemeProvider } from "styled-components";
-import { Button, Input, Rotate, StyledLink } from "../style/dashboard";
+import {
+  Button,
+  Input,
+  Paragraph,
+  Rotate,
+  SecondButton,
+  StyledButton,
+  StyledLink,
+} from "../style/dashboard";
 
 const dashboard = () => {
-  const theme = {
-    main: "#3c3131",
-  };
-
-  Button.defaultProps = {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [show, setShow] = useState(true);
+  StyledButton.defaultProps = {
     theme: {
-      main: "#3c3131",
+      main: "#70b7db",
+      bg: "mediumseagreen",
     },
   };
+
+  const theme = {
+    main: "mediumseagreen",
+  };
+
+  const childTheme = ({ main, bg }) => ({
+    main: bg,
+    bg: main,
+  });
   return (
     <>
       <Button>Button</Button>
@@ -28,8 +44,26 @@ const dashboard = () => {
       <br />
       <Input defaultValue="@geelen" type="text" inputColor="rebeccapurple" />
       <br />
+      <StyledButton>Noraml</StyledButton>
       <ThemeProvider theme={theme}>
-        <Button>Themed</Button>
+        <StyledButton onClick={() => setShow(!show)}>Themed</StyledButton>
+        <br />
+        <Button>Theme</Button>
+        <ThemeProvider theme={childTheme}>
+          <Button>Inverted Theme</Button>
+        </ThemeProvider>
+      </ThemeProvider>
+      <Paragraph show={show}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum
+        provident optio eius quaerat, consequuntur quae ab, aliquam eos
+        recusandae ducimus soluta culpa natus commodi neque sunt nulla quia
+        earum doloremque.
+      </Paragraph>
+      <ThemeProvider>
+        <SecondButton>Second Button</SecondButton>
+        <ThemeProvider>
+          <SecondButton>Second Button</SecondButton>
+        </ThemeProvider>
       </ThemeProvider>
     </>
   );
--- src/style/dashboard.js
+++ src/style/dashboard.js
@@ -6,6 +6,12 @@ const Link = ({ className, children, link }) => (
   </a>
 );
 
+export const StyledLink = styled(Link)`
+  color: palevioletred;
+  font-weight: bold;
+  text-decoration: none;
+`;
+
 const rotate = keyframes`
   from {
     transform: rotate(0deg);
@@ -23,29 +29,54 @@ export const Rotate = styled.div`
   font-size: 1.2rem;
 `;
 
-export const StyledLink = styled(Link)`
-  color: palevioletred;
-  font-weight: bold;
-  text-decoration: none;
-`;
-
 export const Button = styled.button`
-  color:  ${props => props.theme.main};
+  color: ${(props) => props.theme.main};
   background-color: ${(props) => (props.primary ? "#2795f391" : "#2ea72e")};
   border-radius: 5px;
   padding: 5px;
   margin: 10px;
   outline: none;
   cursor: pointer;
-  border: 2px solid ${props => props.theme.main};
+  border: 2px solid ${(props) => props.theme.main};
 `;
 
 export const Input = styled.input`
   padding: 4px;
   margin: 4px;
-  color: ${props => props.inputColor || '#599d11'};
+  color: ${(props) => props.inputColor || "#599d11"};
   background: papayawhip;
   border: none;
   border-radius: 3px;
 `;
 
+export const StyledButton = styled.button`
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+  cursor: pointer;
+  /* Color the border and text with theme.main */
+  color: ${(props) => props.theme.main};
+  border: 2px solid ${(props) => props.theme.main};
+`;
+
+export const SecondButton = styled.button`
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+  border: ${props => props.theme.color};
+  color: ${props => props.theme.color};
+  background-color: ${props => props.theme.bg};;
+`;
+
+export const Paragraph = styled.p`
+  display: ${props => props.show ? 'block' : 'none'};
+  height: 100px;
+  transition: height 2s linear;
+  overflow: hidden;
+
+  &:hover {
+    height: 0px;
+  }
+`;  
